13b2b399ebbcadb870c9eee797448fc797c3dadb
Satoshi-Sh <satoss1108@gmail.com>
2022-09-12 17:12:42 -0500

    Create project object and layout for folders page.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,91 @@
-import _ from 'lodash';
+import _, { divide } from 'lodash';
 import './style.css';
-import Icon from './images/man.png'
-import hello from './project.js'
 
-
-hello()
-
-console.log("Hello todo.")
-
+import {Account} from './project.js'
 
 const content = document.querySelector("#content");
-console.log(content)
-const imageOne = new Image();
-imageOne.src = Icon;
-
-content.appendChild(imageOne)
 
 
+// Create sideBar
+const side = document.createElement('div');
+side.classList.add('side-div')
+side.innerHTML = `<h3 class="title">ToDo</h3>
+<ul class="actions">
+ <li class="add">Add New Folder</li>
+ <li class="delete">Delete Folder</li>
+ </ul>`
+
+content.appendChild(side)
+
+
+// create board 
+const board = document.createElement('div');
+board.classList.add('board')
+
+
+
+// folders function 
+function createCard(t){
+const folder = document.createElement('div')
+folder.classList.add('folder-card')
+
+//title div
+const titleDiv = document.createElement('div')
+titleDiv.classList.add('title-div')
+titleDiv.innerHTML = `<div class="title-line"><i id="folder" class="bi bi-folder-fill zoom"></i><h2>${t}</h2></div>`
+folder.appendChild(titleDiv)
+
+//desc div
+const descDiv = document.createElement('div')
+descDiv.classList.add('desc-div')
+folder.appendChild(descDiv)
+
+//make buttons
+function makeButton(text,tagName) {
+    const b = document.createElement('button');
+    b.innerText=text
+    b.classList.add('btn');
+    b.classList.add('zoom');
+    b.setAttribute('id',tagName) ;
+    descDiv.appendChild(b)
+    
+}
+makeButton("Open", 'open')
+makeButton("Change Name",'change')
+makeButton("Delete",'delete')
+
+
+board.appendChild(folder)
+
+
+content.appendChild(board)
+}
+
+
+function cleanBoard(){
+    board.innerHTML =''
+}
+
+
+
+const account = new Account()
+account.addProject('Family')
+account.addProject('Work')
+account.addProject('Hobby')
+
+
+function showFolders(){
+    for (let i=0;i<account.projects.length;i++){
+        createCard(account.projects[i].folderName);
+    }
+}
+
+showFolders()
+
+
+
+// console.log(account.projects)
+
+// account.projects[0].changeName('Exercise')
+
+// console.log(account.projects)
--- src/project.js
+++ src/project.js
@@ -1,3 +1,26 @@
-export default function hello(){
-console.log('Hello project. test test')
+export const Account = function(){
+    let projects = [];
+    //create project
+    function makeProject(folderName){
+        return {
+        toDOs :[],
+        folderName:folderName,
+        changeName(newName) {
+          return  this.folderName = newName
+        }
+    }
 }
+
+    function addProject(name){
+        const pro = new makeProject(name)
+        projects.push(pro)
+    }
+    //account has a default folder at first 
+    addProject('default')
+    
+    return {
+        projects,addProject
+    }
+
+}
+
